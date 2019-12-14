563df11bf5cb6aa1e159813dd4b419281a8798ad
M'hamed HLIOUI <hmrocket@gmail.com>
2019-12-14 12:13:00 -0500

    init commit

---
+++ app/src/main/java/com/hmrocket/mobilrlivehr/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.hmrocket.mobilrlivehr.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
